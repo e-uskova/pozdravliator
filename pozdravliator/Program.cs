50e7d5eef1f214c3b13961c921be1eb6aea2a376
euskova <euskova@turbosolution.ru>
2023-06-27 16:48:28 +0300

    Basic project

---
+++ pozdravliator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace pozdravliator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
